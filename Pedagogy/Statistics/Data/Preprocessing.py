ecfac327c45f9087339789bef5c7331a30391692
DominiqueMakowski <dom.mak19@gmail.com>
2017-04-22 21:58:02 +0200

    initial data

---
+++ Pedagogy/Statistics/Data/Preprocessing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+files = []
+for file in os.listdir("./Raw/"):
+    file = pd.read_excel("./Raw/" + file)
+    file = file.drop("Teacher", axis=1)
+    files.append(file)
+
+df = pd.concat(files)
+df.to_csv("df.csv", index=False)
